44d8ff0b142900c68b0db65ddc5c1ae669ea6d10
mdoulahi <subzero@localhost.localdomain>
2023-08-08 01:37:27 +0100

    done

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -1,18 +1,17 @@
 #include "libft.h"
 
-char	*ft_strncpy(char *dest, const char *src, size_t n)
+char	*ft_strcpy(char *dest, const char *src, size_t n)
 {
 	size_t	len;
 	size_t	i;
 
 	i = 0;
 	len = ft_strlen(src);
-	while (src[i] && i < n)
+	while (i < len)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	while (i < n)
-		dest[i++] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
--- libft.h
+++ libft.h
@@ -14,5 +14,6 @@ void    *ft_memset(void *str, int c, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s);
 char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strncpy(char *dest, const char *src, size_t n);
 
 #endif

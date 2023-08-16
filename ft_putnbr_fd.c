8bb3976f7e36d3bdbef8b88d1a55c6f71875bc67
Cypher Nex <cypher-nex@localhost.localdomain>
2023-08-16 01:13:01 +0100

    done

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+	}
+	else
+	{
+		if (n < 0)
+		{
+			ft_putchar('-');
+			n *= -1;
+		}
+		if (n >= 10)
+		{
+			ft_putnbr(n / 10);
+			ft_putnbr(n % 10);
+		}
+		else
+			ft_putchar_fd(n + '0', fd);
+	}
+}
